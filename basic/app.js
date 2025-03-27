7548697db13e968902e674ade235874c6dc4b649
Ken Nguyen <46609017+kennn1510@users.noreply.github.com>
2025-03-27 05:27:35 -0500

    add navbar and user list to homepage; update index view to render links and users

--- basic/app.js
+++ basic/app.js
@@ -17,8 +17,15 @@ const path = require("node:path");
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+const links = [
+  { href: "/", text: "Home" },
+  { href: "about", text: "About" },
+];
+
+const users = ["Rose", "Cake", "Biff"];
+
 app.get("/", (req, res) => {
-  res.render("index", { message: "EJS rocks!" });
+  res.render("index", { links: links, users: users });
 });
 
 const PORT = 3000;
